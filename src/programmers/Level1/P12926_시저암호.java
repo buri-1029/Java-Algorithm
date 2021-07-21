e86c8981bb7b1f4551843d4d5f1bfddc46ba11b9
김부희 <kimbuhui@naver.com>
2021-07-22 02:24:46 +0900

    Update P12926_시저암호.java

--- src/programmers/Level1/P12926_시저암호.java
+++ src/programmers/Level1/P12926_시저암호.java
@@ -18,7 +18,7 @@ public class P12926_시저암호 {
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == ' '){
                 answer += " ";
-            }else if(65 <= (int)s.charAt(i) && (int)s.charAt(i) <= 90) { // 대문자
+            }else if('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { // 대문자
                 char ch = (char) (((int)s.charAt(i) + n - 65) % 26 + 65);
                 answer += Character.toString(ch);
             }else{ // 소문자
