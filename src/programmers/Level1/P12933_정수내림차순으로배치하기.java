1ad0d3e8f7e0ca4d02174fda2ccbb92d606a852e
김부희 <kimbuhui@naver.com>
2021-07-22 00:46:54 +0900

    Update P12933_정수내림차순으로배치하기 .java

---
+++ src/programmers/Level1/P12933_정수내림차순으로배치하기.java
@@ -0,0 +1,24 @@
+package programmers.Level1;
+
+import java.util.Arrays;
+
+public class P12933_정수내림차순으로배치하기 {
+    public static void main(String[] args) {
+        long n = 118372;
+        long answer = solution1(n);
+        System.out.println(answer);
+
+    }
+
+    private static long solution1(long n) {
+        long answer = 0;
+        String[] tmp = String.valueOf(n).split("");
+        Arrays.sort(tmp);
+        String str = "";
+        for(int i = tmp.length - 1; i >=0 ; i--){
+            str += tmp[i];
+        }
+        answer = Long.parseLong(str);
+        return answer;
+    }
+}
