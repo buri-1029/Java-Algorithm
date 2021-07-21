1af512188bd59db6f8a74518996047597df56382
김부희 <kimbuhui@naver.com>
2021-07-22 01:01:15 +0900

    Update P12915_문자열내마음대로정렬하기.java

--- src/programmers/Level1/P12915_문자열내마음대로정렬하기.java
+++ src/programmers/Level1/P12915_문자열내마음대로정렬하기.java
@@ -25,9 +25,9 @@ public class P12915_문자열내마음대로정렬하기 {
                 char c1 = s1.charAt(n);
                 char c2 = s2.charAt(n);
 
-                if(c1 == c2){
+                if(c1 == c2){ // n 자리의 문자가 같으면 문자열 자체를 비교
                     return s1.compareTo(s2);
-                } else return c1 - c2;
+                } else return c1 - c2; // 아니면 해당 문자를 비교
             }
         });
         return strings;
