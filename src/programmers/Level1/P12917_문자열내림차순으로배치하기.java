4905b83496ee915ce218b5ac84e9ab9c9b017973
김부희 <kimbuhui@naver.com>
2021-07-22 02:12:14 +0900

    Update P12917_문자열내림차순으로배치하기.java

--- src/programmers/Level1/P12917_문자열내림차순으로배치하기.java
+++ src/programmers/Level1/P12917_문자열내림차순으로배치하기.java
@@ -14,9 +14,14 @@ public class P12917_문자열내림차순으로배치하기 {
         String answer = "";
 
         String[] tmp = String.valueOf(s).split("");
-        Arrays.sort(tmp, Collections.reverseOrder());
-        answer = String.join("", tmp);
+        Arrays.sort(tmp);
 
+        for(int i = tmp.length - 1; i >= 0; i--){
+            answer += tmp[i];
+        }
+
+//        Arrays.sort(tmp, Collections.reverseOrder());
+//        answer = String.join("", tmp);
         return answer;
     }
 
