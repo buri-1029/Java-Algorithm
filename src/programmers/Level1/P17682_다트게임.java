fe02b108b4e42476b343f326e6d0ce165a4bccca
김부희 <kimbuhui@naver.com>
2021-07-22 17:01:08 +0900

    Update P17682_다트게임.java

--- src/programmers/Level1/P17682_다트게임.java
+++ src/programmers/Level1/P17682_다트게임.java
@@ -18,36 +18,44 @@ public class P17682_다트게임 {
     }
     
     static public int solution(String dartResult) {
-        int idx = 0;
-        int[] ans = new int[3];
-        int answer = 0;
+        // dartResult == "점수|보너스|[옵션]"
+        int cur = 0; // 현재 몇 번째 기회인지
+        int[] ans = new int[3]; // 3번의 기회
+
         for (int i = 0; i < dartResult.length(); i++) {
+            // 숫자인지 판별 후
             if (Character.isDigit(dartResult.charAt(i))) {
-                idx++;
-                if (i + 1 < dartResult.length() && Character.isDigit(dartResult.charAt(i + 1))) {
+                cur++;  // 숫자이면 기회 증가
+                // 그 다음 인덱스도 숫자이면 10이기 때문에 인덱스 증가
+                if (Character.isDigit(dartResult.charAt(i + 1))) {
                     i++;
-                    ans[idx - 1] = 10;
+                    ans[cur - 1] = 10;
                 } else {
-                    ans[idx - 1] = dartResult.charAt(i) - '0';
+                    ans[cur - 1] = dartResult.charAt(i) - '0';
                 }
+            // S, D, T 일 때 해당 기회의 점수 제곱으로 계산
             } else if (dartResult.charAt(i) == 'S') {
-                ans[idx - 1] = (int) Math.pow(ans[idx - 1], 1);
+                ans[cur - 1] = (int) Math.pow(ans[cur - 1], 1);
             } else if (dartResult.charAt(i) == 'D') {
-                ans[idx - 1] = (int) Math.pow(ans[idx - 1], 2);
+                ans[cur - 1] = (int) Math.pow(ans[cur - 1], 2);
             } else if (dartResult.charAt(i) == 'T') {
-                ans[idx - 1] = (int) Math.pow(ans[idx - 1], 3);
+                ans[cur - 1] = (int) Math.pow(ans[cur - 1], 3);
+            // * 일 때
             } else if (dartResult.charAt(i) == '*') {
-                if (idx >= 2) {
-                    ans[idx - 1] = ans[idx - 1] * 2;
-                    ans[idx - 2] = ans[idx - 2] * 2;
-                } else {
-                    ans[idx - 1] = ans[idx - 1] * 2;
+                // 중첩되기 때문에 전 기회의 점수 2배로 계산
+                if (cur == 3 || cur == 2) {
+                    ans[cur - 2] = ans[cur - 2] * 2;
                 }
+                // 해당 기회의 점수에서도 2배로 계산
+                ans[cur - 1] = ans[cur - 1] * 2;
+            // # 일 때
             } else if (dartResult.charAt(i) == '#') {
-                ans[idx - 1] = ans[idx - 1] * (-1);
+                // 해당 기회의 점수 마이너스로
+                ans[cur - 1] = ans[cur - 1] * (-1);
             }
         }
 
+        int answer = 0;
         for (int i = 0; i < 3; i++) {
             answer += ans[i];
         }
