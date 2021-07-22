3483704e9fb2b20d968b32f32b5660775a24d58e
김부희 <kimbuhui@naver.com>
2021-07-22 14:08:28 +0900

    Update P12943_콜라츠추측.java

--- src/programmers/Level1/P12943_콜라츠추측.java
+++ src/programmers/Level1/P12943_콜라츠추측.java
@@ -1,24 +1,23 @@
 package programmers.Level1;
 
 public class P12943_콜라츠추측 {
-    public int solution(int num) {
-        int answer = -1;
-
-        int cnt = 0;
+    public static void main(String[] args) {
+        int n = 626331;
+        int ans = solution(n);
+        System.out.println(ans);
+    }
+    public static int solution(int num) {
+        int answer = -1, cnt = 0;
         while(true){
             if(num == 1 || cnt >= 483) break;
 
-            if(num%2 == 0)
-                num = num/2;
-            else{
-                num = num*3 + 1;
-            }
+            if(num%2 == 0) num = num/2;
+            else num = num*3 + 1;
 
             cnt++;
         }
 
-        if(cnt < 483)
-            answer = cnt;
+        if(cnt < 483) answer = cnt;
 
         return answer;
     }
