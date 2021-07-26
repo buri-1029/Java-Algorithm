0098bb912ed2e7cc90f7e5abda79423c79a9fa05
김부희 <kimbuhui@naver.com>
2021-07-27 02:41:40 +0900

    Update P64061_크레인인형뽑기게임.java

--- src/programmers/Level1/P64061_크레인인형뽑기게임.java
+++ src/programmers/Level1/P64061_크레인인형뽑기게임.java
@@ -16,19 +16,22 @@ public class P64061_크레인인형뽑기게임 {
         Stack<Integer> basket = new Stack<>(); // 인형을 담을 바구니
         for(int m = 0; m < moves.length; m++){
             for(int i = 0; i < board.length; i++){
+                // 인형이 있다면
                 if(board[i][moves[m] - 1] != 0){
+                    // 바구니가 비어있지 않고 뽑힌 인형이 이전과 같은 모양의 인형이라면
                     if(!basket.isEmpty() && basket.peek() == board[i][moves[m] - 1]){
+                        // 이전의 인형 제거, 사라진 인형 개수 +2
                         basket.pop();
-                        answer+=2;
+                        answer += 2;
                     }else{
                         basket.push(board[i][moves[m] - 1]);
                     }
+                    // 뽑힌 인형은 빈칸으로 표시
                     board[i][moves[m] - 1] = 0;
                     break;
                 }
             }
         }
-
         return answer;
     }
 }
