f4b0f7235e696681772132ecc8e761ba86888023
김부희 <kimbuhui@naver.com>
2021-11-10 21:14:21 +0900

    Feat P86051_없는숫자더하기

---
+++ src/programmers/Level1/P86051_없는숫자더하기.java
@@ -0,0 +1,24 @@
+package programmers.Level1;
+
+public class P86051_없는숫자더하기 {
+    public static void main(String[] args) {
+        int[] numbers1 = {1,2,3,4,6,7,8,0};
+        int[] numbers2 = {5,8,4,0,6,7,9};
+
+        int answer1 = solution(numbers1);
+        System.out.println(answer1);
+
+        int answer2 = solution(numbers2);
+        System.out.println(answer2);
+    }
+
+    private static int solution(int[] numbers) {
+        int answer = 45;
+
+        for(int i : numbers){
+            answer -= i;
+        }
+
+        return answer;
+    }
+}
