02cf18d791a54f5fbad81ebc2176f42989c01a55
김부희 <kimbuhui@naver.com>
2022-01-04 23:50:08 +0900

    Move P82612_부족한금액계산하기 패키지 이동

---
+++ src/programmers/Level1/P82612_부족한금액계산하기.java
@@ -0,0 +1,23 @@
+package programmers.Level1;
+
+public class P82612_부족한금액계산하기 {
+
+	public static void main(String[] args) {
+		int price = 3;
+		int money = 20;
+		int count = 4;
+
+		long result = solution(price, money, count);
+		System.out.println(result);
+	}
+
+	private static long solution(int price, int money, int count) {
+		int N = 0;
+		long needMoney = 0;
+		while (count != N) {
+			needMoney += (price * ++N);
+		}
+		long answer = (needMoney - money) <= 0 ? 0 : (needMoney - money);
+		return answer;
+	}
+}
