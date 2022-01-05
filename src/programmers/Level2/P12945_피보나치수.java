c83f78ce08bdfae006847641d3b4f7a1635f5c78
김부희 <kimbuhui@naver.com>
2022-01-06 03:13:58 +0900

    Feat P12945_피보나치수.java
    - 재귀로 풀었더니 시간초과가 떠서 디피로 풀었당

---
+++ src/programmers/Level2/P12945_피보나치수.java
@@ -0,0 +1,23 @@
+package programmers.Level2;
+
+public class P12945_피보나치수 {
+
+	public static void main(String[] args) {
+		int n = 1234568;
+		System.out.println(solution(n));
+	}
+
+	private static int solution(int n) {
+		int[] fibo = new int[n + 1];
+
+		fibo[0] = 0;
+		fibo[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			fibo[i] = (fibo[i - 2] % 1234567) + (fibo[i - 1] % 1234567);
+		}
+
+		return fibo[n] % 1234567;
+	}
+
+}
