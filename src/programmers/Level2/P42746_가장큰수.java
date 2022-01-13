cf0cd07d6491ce2470d267292191ba2d9a419ae7
김부희 <kimbuhui@naver.com>
2022-01-13 18:23:42 +0900

    Update P42746_가장큰수.java
    - comparator로 정렬하여 푸는 방법으로 변경

--- src/programmers/Level2/P42746_가장큰수.java
+++ src/programmers/Level2/P42746_가장큰수.java
@@ -1,10 +1,9 @@
 package programmers.Level2;
 
-public class P42746_가장큰수 {
+import java.util.Arrays;
+import java.util.Comparator;
 
-	static int[] select;
-	static boolean[] visit;
-	static int maxValue;
+public class P42746_가장큰수 {
 
 	public static void main(String[] args) {
 		int[] numbers = {6, 10, 2};
@@ -15,33 +14,29 @@ public class P42746_가장큰수 {
 	}
 
 	private static String solution(int[] numbers) {
-		select = new int[numbers.length];
-		visit = new boolean[numbers.length];
-		maxValue = 0;
+		String answer = "";
+		String[] strings = new String[numbers.length];
 
-		permutation(0, numbers);
-
-		return String.valueOf(maxValue);
-	}
-
-	private static void permutation(int k, int[] numbers) {
-		if (k == select.length) {
-			String tmp = "";
-			for (int i : select) {
-				tmp += String.valueOf(i);
-			}
-			maxValue = Math.max(maxValue, Integer.valueOf(tmp));
-			return;
+		for (int i = 0; i < strings.length; i++) {
+			strings[i] = String.valueOf(numbers[i]);
 		}
 
-		for (int i = 0; i < numbers.length; i++) {
-			if (!visit[i]) {
-				visit[i] = true;
-				select[k] = numbers[i];
-				permutation(k + 1, numbers);
-				visit[i] = false;
+		Arrays.sort(strings, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return ((o2 + o1).compareTo(o1 + o2));
 			}
+		});
+
+		if ("0".equals(strings[0])) {
+			return "0";
 		}
+
+		for (String str : strings) {
+			answer += str;
+		}
+
+		return answer;
 	}
 
 }
