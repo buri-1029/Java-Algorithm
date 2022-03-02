0e94fa24e9efdcc11d9386bbb2cf5f7bd381f51a
김부희 <kimbuhui@naver.com>
2022-03-03 03:50:19 +0900

    Feat B2675_문자열반복.java

---
+++ src/baek/bronze/B2675_문자열반복.java
@@ -0,0 +1,23 @@
+package baek.bronze;
+
+import java.util.Scanner;
+
+public class B2675_문자열반복 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+
+		for (int t = 0; t < T; t++) {
+			int R = scan.nextInt();
+			String S = scan.next();
+
+			for (int i = 0; i < S.length(); i++) {
+				for (int j = 0; j < R; j++) {
+					System.out.print(S.charAt(i));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
