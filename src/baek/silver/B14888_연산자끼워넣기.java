bbf3022d18b9c79314b02906518e8609e19798ca
김부희 <kimbuhui@naver.com>
2022-03-09 23:47:05 +0900

    Update B14888_연산자끼워넣기.java
    - 재귀 다시 풀어보기

--- src/baek/silver/B14888_연산자끼워넣기.java
+++ src/baek/silver/B14888_연산자끼워넣기.java
@@ -8,49 +8,51 @@ import java.util.Scanner;
 public class B14888_연산자끼워넣기 {
 
 	static int N;
-	static int[] num;
+	static int[] nums;
 	static int max = Integer.MIN_VALUE;
 	static int min = Integer.MAX_VALUE;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		num = new int[N];
-		int[] op = new int[4];
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+
+		nums = new int[N];
+		int[] calc = new int[4];
 
 		for (int i = 0; i < N; i++) {
-			num[i] = sc.nextInt(); //숫자 배열에 입력
+			nums[i] = scan.nextInt();
 		}
 
-		for (int i = 0; i < op.length; i++) {
-			op[i] = sc.nextInt(); //연산자 수 배열에 입력 
-		}                         //0: +, 1: -, 2: *, 3: /
+		for (int i = 0; i < 4; i++) {
+			calc[i] = scan.nextInt();
+		}
 
-		/** *************** */
-		calculate(1, num[0], op[0], op[1], op[2], op[3]); //재귀 
+		calculate(1, nums[0], calc[0], calc[1], calc[2], calc[3]);
+
 		System.out.println(max);
 		System.out.println(min);
 	}
 
-	private static void calculate(int idx, int sum, int op1, int op2, int op3, int op4) {
-		// TODO Auto-generated method stub
+	private static void calculate(int idx, int sum, int c1, int c2, int c3, int c4) {
 		if (idx == N) {
 			max = Math.max(max, sum);
 			min = Math.min(min, sum);
 		}
 
-		if (op1 > 0) { // + 연산 있으면 그 다음 숫자 인덱스+1, sum에도 그만큼 , + 연산 사용했으니 -1,,,
-			calculate(idx + 1, sum + num[idx], op1 - 1, op2, op3, op4);
+		if (c1 > 0) {
+			calculate(idx + 1, sum + nums[idx], c1 - 1, c2, c3, c4);
 		}
-		if (op2 > 0) {
-			calculate(idx + 1, sum - num[idx], op1, op2 - 1, op3, op4);
+
+		if (c2 > 0) {
+			calculate(idx + 1, sum - nums[idx], c1, c2 - 1, c3, c4);
 		}
-		if (op3 > 0) {
-			calculate(idx + 1, sum * num[idx], op1, op2, op3 - 1, op4);
+
+		if (c3 > 0) {
+			calculate(idx + 1, sum * nums[idx], c1, c2, c3 - 1, c4);
 		}
-		if (op4 > 0) {
-			calculate(idx + 1, sum / num[idx], op1, op2, op3, op4 - 1);
+
+		if (c4 > 0) {
+			calculate(idx + 1, sum / nums[idx], c1, c2, c3, c4 - 1);
 		}
 	}
 }
