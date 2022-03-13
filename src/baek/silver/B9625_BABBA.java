2c356346edd456425e2e69eefb1f6f5554709c41
김부희 <kimbuhui@naver.com>
2022-03-13 23:53:41 +0900

    Feat B9625_BABBA.java

---
+++ src/baek/silver/B9625_BABBA.java
@@ -0,0 +1,28 @@
+package baek.silver;
+
+import java.util.Scanner;
+
+public class B9625_BABBA {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int K = scan.nextInt();
+
+		int[] A = new int[K + 1];
+		int[] B = new int[K + 1];
+
+		A[0] = 1;
+		A[1] = 0;
+
+		B[0] = 0;
+		B[1] = 1;
+
+		for (int i = 2; i < K + 1; i++) {
+			A[i] = B[i - 1];
+			B[i] = B[i - 2] + B[i - 1];
+		}
+
+		System.out.println(A[K] + " " + B[K]);
+	}
+
+}
