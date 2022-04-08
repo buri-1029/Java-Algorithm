3985ef7cfb9411fb81029dda905457b73e287a3a
buri-1029 <kimbuhui@naver.com>
2022-04-08 23:31:58 +0900

    Update B2304_창고다각형.java

--- src/baek/silver/B2304_창고다각형.java
+++ src/baek/silver/B2304_창고다각형.java
@@ -4,33 +4,39 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-/*
-    9%부터 틀린다..stack으로 풀어야하나...
- */
 public class B2304_창고다각형 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int area = 0;
+
 		ArrayList<Info> arr = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
 			arr.add(new Info(sc.nextInt(), sc.nextInt()));
 		}
 		Collections.sort(arr);
 
+		int max = 0;
+
 		Info before = arr.get(0);
 		for (int i = 1; i < arr.size(); i++) {
 			if (before.h <= arr.get(i).h) {
 				area += (arr.get(i).l - before.l) * before.h;
 				before = arr.get(i);
-			}
-			if (i == arr.size() - 1) {
-				int tmp1 = (arr.get(i).l - before.l + 1) * before.h;
-				int tmp2 = (before.h - arr.get(i).h) * (arr.get(i).l - before.l);
-				area += (tmp1 - tmp2);
+				max = i;
 			}
 		}
+
+		before = arr.get(N - 1);
+		for (int i = 0; i < N - max; i++) {
+			if (before.h <= arr.get(N - i - 1).h) {
+				area += (before.l - arr.get(N - i - 1).l) * before.h;
+				before = arr.get(N - i - 1);
+			}
+		}
+
+		area += before.h;
 		System.out.println(area);
 	}
 
@@ -45,12 +51,7 @@ public class B2304_창고다각형 {
 
 		@Override
 		public int compareTo(Info o) {
-			if (this.l < o.l) {
-				return -1;
-			} else if (this.l > o.l) {
-				return 1;
-			}
-			return 0;
+			return this.l - o.l;
 		}
 	}
 }
