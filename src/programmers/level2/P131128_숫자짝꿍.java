21e61a35045f4c5e510e2277347785ef856bee98
buri <kimbuhui@naver.com>
2022-10-12 22:37:33 +0900

    Update P131128_숫자짝꿍.java

--- src/programmers/level2/P131128_숫자짝꿍.java
+++ src/programmers/level2/P131128_숫자짝꿍.java
@@ -4,33 +4,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/*
+	X, Y 중 하나를 기준을 잡는 방법이 아니라
+	숫자 0 ~ 9를 이용하는 방법으로 변경,,,
+ */
 public class P131128_숫자짝꿍 {
 
 	public static void main(String[] args) {
 		String x1 = "100";
-		String y1 = "2345";
+		String y1 = "203045";
 
 		String x2 = "5525";
 		String y2 = "1255";
 
 		System.out.println(solution(x1, y1));
 		System.out.println(solution(x2, y2));
+
 	}
 
 	public static String solution(String X, String Y) {
 		List<String> list = new ArrayList<>();
 
-		String shortStr = X.length() <= Y.length() ? X : Y;
-		String longStr = X.length() <= Y.length() ? Y : X;
+		for (int i = 0; i <= 9; i++) {
+			String tmp = String.valueOf(i);
 
-		for (char c : shortStr.toCharArray()) {
-			String tmp = Character.toString(c);
+			if (X.contains(tmp) && Y.contains(tmp)) {
 
-			if (longStr.contains(tmp)) {
-				list.add(tmp);
-				longStr = longStr.replaceFirst(tmp, "");
+				int x = countChar(X, tmp);
+				int y = countChar(Y, tmp);
+
+				int min = Math.min(x, y);
+
+				for (int j = 0; j < min; j++) {
+					list.add(tmp);
+				}
 			}
-
 		}
 
 		list.sort(Collections.reverseOrder());
@@ -41,11 +49,17 @@ public class P131128_숫자짝꿍 {
 
 		String answer = String.join("", list);
 
-		if (answer.charAt(0) == 0) {
+		if (answer.charAt(0) == '0') {
 			return "0";
 		}
 
 		return answer;
 	}
+
+	public static int countChar(String str, String s) {
+		return str.length() - str.replace(s, "")
+								 .length();
+	}
+
 
 }
