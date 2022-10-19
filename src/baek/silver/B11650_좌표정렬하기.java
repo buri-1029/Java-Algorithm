ec2cb5ee5e96c948fc4cf8e95d3e389d50854e90
buri <kimbuhui@naver.com>
2022-10-19 23:23:46 +0900

    Feat B11650_좌표정렬하기.java

---
+++ src/baek/silver/B11650_좌표정렬하기.java
@@ -0,0 +1,45 @@
+package baek.silver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class B11650_좌표정렬하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		List<Coordinate> list = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			list.add(new Coordinate(sc.nextInt(), sc.nextInt()));
+		}
+
+		Collections.sort(list);
+
+		for (Coordinate c : list) {
+			System.out.println(c.x + " " + c.y);
+		}
+	}
+
+	static class Coordinate implements Comparable<Coordinate> {
+
+		int x;
+		int y;
+
+		public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Coordinate o) {
+			if (o.x == this.x) {
+				return this.y - o.y;
+			}
+			return this.x - o.x;
+		}
+	}
+}
