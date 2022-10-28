a417887e622d0dcf0ea14db6e85cd7c7eccd31bd
buri <kimbuhui@naver.com>
2022-10-28 11:27:54 +0900

    Feat B5555_반지.java

---
+++ src/baek/silver/B5555_반지.java
@@ -0,0 +1,26 @@
+package baek.silver;
+
+import java.util.Scanner;
+
+public class B5555_반지 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int n = sc.nextInt();
+
+		int cnt = 0;
+
+		for (int i = 0; i < n; i++) {
+			String s = sc.next();
+			s += s;
+
+			if (s.contains(str)) {
+				cnt++;
+			}
+		}
+
+		System.out.println(cnt);
+	}
+
+}
