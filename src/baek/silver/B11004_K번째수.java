395d8edf828f27c888d1992114649abc47fb99fb
buri <kimbuhui@naver.com>
2022-11-01 23:21:35 +0900

    Feat B11004_K번째수.java

---
+++ src/baek/silver/B11004_K번째수.java
@@ -0,0 +1,36 @@
+package baek.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class B11004_K번째수 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int target = Integer.parseInt(st.nextToken());
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+
+		Collections.sort(list);
+
+		bw.write(String.valueOf(list.get(target - 1)));
+
+		br.close();
+		bw.close();
+	}
+}
