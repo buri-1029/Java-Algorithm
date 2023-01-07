f21fe7796c930837f21a5915ab66979cb4cc079b
buri <kimbuhui@naver.com>
2023-01-07 23:30:08 +0900

    Feat B10988_팰린드롬인지확인하기.java

---
+++ src/baek/bronze/B10988_팰린드롬인지확인하기.java
@@ -0,0 +1,21 @@
+package baek.bronze;
+
+import java.util.Scanner;
+
+public class B10988_팰린드롬인지확인하기 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+
+		int answer = 1;
+		for (int i = 0; i < str.length() / 2; i++) {
+			if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+				answer = 0;
+				break;
+			}
+		}
+
+		System.out.println(answer);
+	}
+}
