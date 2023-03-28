0c1b0a46110ca41fd5e3142246ef25c638c10fa3
buri <kimbuhui@naver.com>
2023-03-28 23:09:59 +0900

    Update P77885_2개이하로다른비트.java

--- src/programmers/level2/P77885_2개이하로다른비트.java
+++ src/programmers/level2/P77885_2개이하로다른비트.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class P77885_2개이하로다른비트 {
 
 	public static void main(String[] args) {
-		long[] numbers = {2, 7};
+		long[] numbers = {2, 45};
 		System.out.println(Arrays.toString(solution(numbers)));
 
 	}
@@ -14,32 +14,22 @@ public class P77885_2개이하로다른비트 {
 		long[] answer = new long[numbers.length];
 
 		for (int i = 0; i < numbers.length; i++) {
-			String str = Long.toBinaryString(numbers[i]);
-
 			if (numbers[i] % 2 == 0) {
 				answer[i] = numbers[i] + 1;
 			} else {
-				int last = str.lastIndexOf("0");
-				int first = str.indexOf("1", last);
+				String str = Long.toBinaryString(numbers[i]);
+				int idx = str.lastIndexOf("0");
 
-				if (last == -1) {
-					numbers[i] += 1;
-					str = Long.toBinaryString(numbers[i]);
-					str = str.substring(0, 2) +
-						str.substring(2)
-						   .replace("0", "1");
-
+				if (idx == -1) {
+					str = "10" + str.substring(1);
 				} else {
-					str = str.substring(0, last) + "1" +
-						str.substring(last + 1, first) + "0" +
-						str.substring(first + 1);
+					str = str.substring(0, idx) + "10" + str.substring(idx + 2);
 				}
 
 				answer[i] = Long.parseLong(str, 2);
 			}
-
-
 		}
+
 		return answer;
 	}
 
